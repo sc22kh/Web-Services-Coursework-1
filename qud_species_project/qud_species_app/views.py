e028c3f7e06d4dc8bb0a7b46bdf79daabb82ae7d
sc22kh <sc22kh@leeds.ac.uk>
2026-03-13 17:49:48 +0000

    feat: added API endpoints for BodyParts, Mutations, Skills, and Anatomies

--- qud_species_project/qud_species_app/views.py
+++ qud_species_project/qud_species_app/views.py
@@ -1,7 +1,23 @@
 from rest_framework import viewsets
-from .models import Creature
-from .serializers import CreatureSerializer
+from .models import Creature, Skill, Mutation, Anatomy, BodyPart
+from .serializers import CreatureSerializer, SkillSerializer, MutationSerializer, AnatomySerializer, BodyPartSerializer
 
 class CreatureViewSet(viewsets.ModelViewSet):
     queryset = Creature.objects.all()
     serializer_class = CreatureSerializer
+
+class SkillViewSet(viewsets.ModelViewSet):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+
+class MutationViewSet(viewsets.ModelViewSet):
+    queryset = Mutation.objects.all()
+    serializer_class = MutationSerializer
+
+class AnatomyViewSet(viewsets.ModelViewSet):
+    queryset = Anatomy.objects.all()
+    serializer_class = AnatomySerializer
+
+class BodyPartViewSet(viewsets.ModelViewSet):
+    queryset = BodyPart.objects.all()
+    serializer_class = BodyPartSerializer
