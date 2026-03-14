f0d200678dfc527d98590370a77ebc073149b2a2
sc22kh <sc22kh@leeds.ac.uk>
2026-03-14 16:07:18 +0000

    fix: removed duplicate ALLOWED_HOSTS

--- qud_species_project/qud_species_project/settings.py
+++ qud_species_project/qud_species_project/settings.py
@@ -27,13 +27,10 @@ ALLOWED_HOSTS = ['kaddykins.eu.pythonanywhere.com']
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
