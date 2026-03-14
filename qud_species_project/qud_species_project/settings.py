a4452a7410c4469202000e3831a5055648cf8db9
sc22kh <sc22kh@leeds.ac.uk>
2026-03-14 04:32:46 +0000

    fix: updated pythonanywhere url to EU server

--- qud_species_project/qud_species_project/settings.py
+++ qud_species_project/qud_species_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static_files'
 
-ALLOWED_HOSTS = ['kaddykins.pythonanywhere.com']
+ALLOWED_HOSTS = ['kaddykins.eu.pythonanywhere.com']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
